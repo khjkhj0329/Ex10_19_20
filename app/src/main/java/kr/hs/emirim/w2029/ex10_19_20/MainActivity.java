a612ff24bb3f41ff0863fdf8e3878fb15a48ed86
효정이 <w2029@e-mirim.hs.kr>
2021-07-20 15:30:29 +0900

    Completion Ex10_19_20 Commit

--- app/src/main/java/kr/hs/emirim/w2029/ex10_19_20/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/w2029/ex10_19_20/MainActivity.java
@@ -2,7 +2,13 @@ package kr.hs.emirim.w2029.ex10_19_20;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.SearchManager;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,54 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnCall = findViewById(R.id.btn_call);
+        Button btnHome = findViewById(R.id.btn_homepage);
+        Button btnMap= findViewById(R.id.btn_map);
+        Button btnSearch = findViewById(R.id.btn_search);
+        Button btnSMS = findViewById(R.id.btn_sms);
+        Button btnPhoto = findViewById(R.id.btn_photo);
+
+        btnCall.setOnClickListener(btnListener);
+        btnHome.setOnClickListener(btnListener);
+        btnMap.setOnClickListener(btnListener);
+        btnSearch.setOnClickListener(btnListener);
+        btnSMS.setOnClickListener(btnListener);
+        btnPhoto.setOnClickListener(btnListener);
     }
+    View.OnClickListener btnListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent = null;
+            Uri uri = null;
+            switch (v.getId()){
+                case R.id.btn_call:
+                    uri = Uri.parse("tel:01050919819");
+                    intent = new Intent(Intent.ACTION_DIAL, uri);
+                    break;
+                case R.id.btn_homepage:
+                    uri = Uri.parse("https://www.e-mirim.hs.kr");
+                    intent = new Intent(Intent.ACTION_VIEW, uri);
+                    break;
+                case R.id.btn_map:
+                    uri = Uri.parse("https://maps.google.co.kr/maps?q="+ 37.668323012353596 + "," + 127.03204592337748 + "&z=15");
+                    intent = new Intent(Intent.ACTION_VIEW, uri);
+                    break;
+                case R.id.btn_search:
+                    intent = new Intent(Intent.ACTION_WEB_SEARCH);
+                    intent.putExtra(SearchManager.QUERY, "코로나 현황");
+                    break;
+                case R.id.btn_sms:
+                    uri = Uri.parse("smsto:" + Uri.encode("010-9382-9819"));
+                    intent = new Intent(Intent.ACTION_SENDTO);
+                    intent.putExtra("sms_body", "연애는 어떻게 하면 좋을까?");
+                    intent.setData(uri);
+                    break;
+                case R.id.btn_photo:
+                    intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    break;
+            }
+            startActivity(intent);
+        }
+    };
 }
